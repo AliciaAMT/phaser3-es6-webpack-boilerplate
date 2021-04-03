266c2b391b150399882a635e742ec67d282e562d
Alicia A Taylor <aliciataylorguitar@gmail.com>
2021-04-03 13:38:39 -0500

    Implement Jumping and Double Jumping

--- src/entities/hero.js
+++ src/entities/hero.js
@@ -13,22 +13,63 @@ class Hero extends Phaser.GameObjects.Sprite {
         this.body.setSize(12, 40);
         this.body.setOffset(12,23);
 
+        this.body.setMaxVelocity(250, 400);
+        this.body.setDragX(750);
+
         this.keys = scene.cursorKeys;
     }
 
     preUpdate(time, delta) {
         super.preUpdate(time, delta);
         if (this.keys.left.isDown) {
-            this.body.setVelocityX(-250);
+            this.body.setAccelerationX(-1000);
             this.setFlipX(true);
             this.body.offset.x = 8;
         } else if (this.keys.right.isDown) {
-            this.body.setVelocityX(250);
+            this.body.setAccelerationX(1000);
             this.setFlipX(false);
             this.body.offset.x = 12;
         } else {
-            this.body.setVelocityX(0);
+            this.body.setAccelerationX(0);
         }
+/* **********OPTIONAL CHANGE JUMP TO REPRESS UP KEY EVERY TIME OR IT WON"T BOUNCE***********
+                Replce line: if(didPressJump && this.body.onFloor()) {
+                With: if(this.keys.up.isDown && this.body.onFloor()) {
+                and remove variable(const) to revert back to holding down up button to bounce
+                Think about allowing bounce to rev up a higher jump...
+
+*/
+                    // set to true if you want to double jump out of falls or knockback
+        if (this.body.onFloor()) {
+            this.canDoubleJump = false;
+        }
+
+        if (this.body.velocity.y > 0) {
+            this.isJumping = false;
+        }
+
+        const didPressJump = Phaser.Input.Keyboard.JustDown(this.keys.up);
+//create double jump shoes named after simone biles the "goat" shoes
+        // if (didPressJump && this.body.onFloor()) {
+        //     this.body.setVelocityY(-400);
+        // }
+        
+        if (didPressJump) {
+            if (this.body.onFloor()) {
+                this.isJumping = true;
+                this.canDoubleJump = true;
+                this.body.setVelocityY(-400);
+            } else if (this.canDoubleJump) {
+                this.isJumping = true;
+                this.canDoubleJump = false;
+                this.body.setVelocityY(-300);
+            }
+        }
+
+        if (!this.keys.up.isDown && this.body.velocity.y < -150 && this.isJumping) {
+            this.body.setVelocityY(-150);
+        }
+        
     }
 }
 export default Hero;
